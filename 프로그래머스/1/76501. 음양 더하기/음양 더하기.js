a9e1d11fbf16746ca096f26803efcf9f167fc993
IlWooChoi <ilwoo.choi.00@gmail.com>
2026-01-07 08:01:08 +0900

    [level 1] Title: 음양 더하기, Time: 0.17 ms, Memory: 33.4 MB -BaekjoonHub

---
+++ 프로그래머스/1/76501. 음양 더하기/음양 더하기.js
@@ -0,0 +1,12 @@
+const solution = (absolutes, signs) => {
+    let sum = 0;
+    for(let i = 0; i < absolutes.length; i++){
+        if(signs[i] === true){
+            sum += absolutes[i];
+        } else {
+            sum -= absolutes[i];
+        }
+    }
+    
+    return sum;
+}
