39e41c411dea51d5438db2ee3d71eb8e07334a44
IlWooChoi <ilwoo.choi.00@gmail.com>
2026-01-07 09:51:26 +0900

    [level 1] Title: 콜라츠 추측, Time: 0.14 ms, Memory: 33.3 MB -BaekjoonHub

---
+++ 프로그래머스/1/12943. 콜라츠 추측/콜라츠 추측.js
@@ -0,0 +1,14 @@
+const solution = (n) => {
+    let count = 0;
+
+    while (n !== 1 && count < 500) {
+        if (n % 2 === 0) {
+            n /= 2;
+        } else {
+            n = n * 3 + 1;
+        }
+        count++;
+    }
+
+    return n === 1 ? count : -1;
+}
