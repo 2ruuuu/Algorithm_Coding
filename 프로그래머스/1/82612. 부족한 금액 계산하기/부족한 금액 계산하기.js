49ccca7fbd98a9a92c6fb4554e22eac3affde6d2
IlWooChoi <ilwoo.choi.00@gmail.com>
2026-01-07 13:11:10 +0900

    [level 1] Title: 부족한 금액 계산하기, Time: 0.11 ms, Memory: 33.5 MB -BaekjoonHub

---
+++ 프로그래머스/1/82612. 부족한 금액 계산하기/부족한 금액 계산하기.js
@@ -0,0 +1,12 @@
+const solution = (price, money, count) => {
+    for(let i = 1; i <= count; i++){
+        money -= price * i;
+    }
+    
+    if(money < 0){
+        return Math.abs(money);
+    } else {
+        return 0;
+    }
+        
+}
