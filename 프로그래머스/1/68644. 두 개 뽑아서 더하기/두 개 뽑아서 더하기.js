8d8c4769b9de526ff1c47985e082e33c6142e07b
IlWooChoi <ilwoo.choi.00@gmail.com>
2026-01-11 13:05:37 +0900

    [level 1] Title: 두 개 뽑아서 더하기, Time: 0.55 ms, Memory: 33.6 MB -BaekjoonHub

--- 프로그래머스/1/68644. 두 개 뽑아서 더하기/두 개 뽑아서 더하기.js
+++ 프로그래머스/1/68644. 두 개 뽑아서 더하기/두 개 뽑아서 더하기.js
@@ -3,10 +3,9 @@ const solution = (numbers) => {
     for(let i = 0; i < numbers.length; i++){
         for(let j = i+1; j < numbers.length; j++){
             let sum = numbers[i] + numbers[j];
-            if(result.includes(sum) === false){
-                result.push(sum);
-            }
+            result.push(sum);
         }
     }
+    result = [...new Set(result)];
     return result.sort((a, b) => a - b);
 }
