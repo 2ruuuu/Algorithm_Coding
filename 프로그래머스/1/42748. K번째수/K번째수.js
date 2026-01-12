5a35213c44a01fcc4b46c10adac056676b91936b
IlWooChoi <ilwoo.choi.00@gmail.com>
2026-01-13 08:34:31 +0900

    [level 1] Title: K번째수, Time: 0.08 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/1/42748. K번째수/K번째수.js
+++ 프로그래머스/1/42748. K번째수/K번째수.js
@@ -1,10 +1,7 @@
 const solution = (array, commands) => {
-    let ans = [];
-    for(let i = 0; i < commands.length; i++){
-        let [first, second, third] = commands[i];
+    return commands.map(([first, second, third]) => {
         let newArr = array.slice(first-1, second);
         newArr.sort((a, b) => a - b);
-        ans.push(newArr[third-1]);
-    }
-    return ans;
+        return newArr[third-1];
+    })
 }
