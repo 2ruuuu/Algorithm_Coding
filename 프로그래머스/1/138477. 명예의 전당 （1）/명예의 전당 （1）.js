04753ac3a670240ef999c3f90d64dc5528a7ead5
IlWooChoi <ilwoo.choi.00@gmail.com>
2026-01-14 07:55:39 +0900

    [level 1] Title: 명예의 전당 (1), Time: 22.96 ms, Memory: 36.5 MB -BaekjoonHub

---
+++ 프로그래머스/1/138477. 명예의 전당 （1）/명예의 전당 （1）.js
@@ -0,0 +1,18 @@
+const solution = (k, score) => {
+    let arr = [];
+    let result = [];
+    for(let i = 0; i < score.length; i++){
+        if(arr.length < k){
+            arr.push(score[i]);
+            arr.sort((a, b) => a - b);
+            result.push(arr[0]);
+        } else{
+            arr.push(score[i]);
+            arr.sort((a, b) => a - b);
+            arr.shift();
+            result.push(arr[0]);
+        }
+    }
+    
+    return result;
+}
