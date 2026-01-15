2231977ab6b497b158dc9c52d7076af553549dd4
IlWooChoi <ilwoo.choi.00@gmail.com>
2026-01-15 16:08:54 +0900

    [level 1] Title: 문자열 내 마음대로 정렬하기, Time: 0.24 ms, Memory: 33.6 MB -BaekjoonHub

--- 프로그래머스/1/12915. 문자열 내 마음대로 정렬하기/문자열 내 마음대로 정렬하기.js
+++ 프로그래머스/1/12915. 문자열 내 마음대로 정렬하기/문자열 내 마음대로 정렬하기.js
@@ -1,19 +1,11 @@
 const solution = (strings, n) => {
-    strings.sort((a, b) => {
-        if(a[n] > b[n]){
-            return 1;
-        }
-        if(a[n] < b[n]){
-            return -1;
-        }
-        if(a > b){
-            return 1;
-        }
-        if(a < b){
-            return -1;
-        }
-        return 0;
-    })
-    
-    return strings;
+    let result = [];
+    for(let i = 0; i < strings.length; i++){
+        let str = strings[i][n] + strings[i];
+        result.push(str);
+    }
+    result.sort();
+    return result.map((e) => {
+        return e.slice(1);
+    });
 }
