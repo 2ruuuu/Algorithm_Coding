a85f668b37edfa5c46c56ef59e0bf80ce47729be
IlWooChoi <ilwoo.choi.00@gmail.com>
2026-01-16 13:35:23 +0900

    [level 1] Title: [1차] 비밀지도, Time: 0.23 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/1/17681. ［1차］ 비밀지도/［1차］ 비밀지도.js
+++ 프로그래머스/1/17681. ［1차］ 비밀지도/［1차］ 비밀지도.js
@@ -1,26 +1,19 @@
-const solution = (n, arr1, arr2) => {    
-    let newArr1 = arr1.map((e) => e.toString(2).padStart(n, 0));
-    let newArr2 = arr2.map((e) => e.toString(2).padStart(n, 0));
-    let newArr3 = [];
+const solution = (n, arr1, arr2) => {
+    const newArr1 = arr1.map((e) => e.toString(2).padStart(n, 0));
+    const newArr2 = arr2.map((e) => e.toString(2).padStart(n, 0));
+    const newArr3 = [];
     
     for(let i = 0; i < n; i++){
         let str = "";
         for(let j = 0; j < n; j++){
-            str += Number(newArr1[i][j])+Number(newArr2[i][j]);
-        }
-        newArr3.push(str);
-    }
-        
-    return newArr3.map((e) => {
-        let str = "";
-        let elem = String(e).split("");
-        for(let i = 0; i < n; i++){
-            if(elem[i] >= 1){
+            if(Number(newArr1[i][j]) + Number(newArr2[i][j]) >= 1){
                 str += "#";
             } else {
                 str += " ";
             }
         }
-        return str;
-    })
+        newArr3.push(str);
+    }
+    
+    return newArr3;
 }
