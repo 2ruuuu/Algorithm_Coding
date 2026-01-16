5a0083734e8c940c2a801b36762eaae15b95d087
IlWooChoi <ilwoo.choi.00@gmail.com>
2026-01-16 16:09:15 +0900

    [level 1] Title: 2016년, Time: 13.14 ms, Memory: 34.5 MB -BaekjoonHub

--- 프로그래머스/1/12901. 2016년/2016년.js
+++ 프로그래머스/1/12901. 2016년/2016년.js
@@ -1,11 +1,5 @@
-function solution(a, b) {
-    const months = [31, 29, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31];
-    const days = ['THU', 'FRI', 'SAT', 'SUN', 'MON', 'TUE', 'WED'];
+const solution = (a, b) => {
+    let date = new Date(2016, a-1, b);
+    return date.toString().slice(0, 3).toUpperCase();
     
-    let totalDays = b;
-    for (let i = 0; i < a - 1; i++) {
-        totalDays += months[i];
-    }
-    
-    return days[totalDays % 7];
 }
