68dd688d88d8beb158445faefecddf4391823073
IlWooChoi <ilwoo.choi.00@gmail.com>
2026-01-22 09:50:05 +0900

    [level 1] Title: 가장 가까운 같은 글자, Time: 4.43 ms, Memory: 37.7 MB -BaekjoonHub

--- 프로그래머스/1/142086. 가장 가까운 같은 글자/가장 가까운 같은 글자.js
+++ 프로그래머스/1/142086. 가장 가까운 같은 글자/가장 가까운 같은 글자.js
@@ -1,23 +1,21 @@
 const solution = (s) => {
     const result = [];
-    for(let i = 0; i < s.length; i++){
-        let count = 0;
-        let j = i
-        for(j = i; j > 0; j--){
-            if(s[i] !== s[j-1]){
-                count++;
-            } else {
-                count++;
-                break;
-            }
-        }
-        if(count === i && j === 0){
-            result.push(-1);
+    const lastSeen = new Map(); // 글자의 마지막 위치를 저장할 저장소
+
+    for (let i = 0; i < s.length; i++) {
+        const char = s[i];
+
+        if (lastSeen.has(char)) {
+            // 이전에 나온 적이 있다면: 현재 위치(i) - 마지막 위치
+            result.push(i - lastSeen.get(char));
         } else {
-            result.push(count);
+            // 처음 나온 글자라면
+            result.push(-1);
         }
-        
+
+        // 현재 글자의 위치를 최신 정보로 업데이트
+        lastSeen.set(char, i);
     }
-    
+
     return result;
-}
+};
