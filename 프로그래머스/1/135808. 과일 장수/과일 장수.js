41761d64095f7abe1aa184a424c72ccc421ad879
IlWooChoi <ilwoo.choi.00@gmail.com>
2026-01-23 11:21:31 +0900

    [level 1] Title: 과일 장수, Time: 232.57 ms, Memory: 83.7 MB -BaekjoonHub

---
+++ 프로그래머스/1/135808. 과일 장수/과일 장수.js
@@ -0,0 +1,8 @@
+const solution = (k, m, score) => {
+    score.sort((a, b) => b - a);
+    let sum = 0;
+    for(let i = 0; i < score.length-(m-1); i+=m){
+        sum += score[i+m-1] * m
+    }
+    return sum;
+}
