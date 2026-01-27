ebfb7fb9b4461e762681cbec6b1c34f1db00026c
IlWooChoi <ilwoo.choi.00@gmail.com>
2026-01-27 16:32:31 +0900

    [level 2] Title: 기능개발, Time: 0.23 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/2/42586. 기능개발/기능개발.js
+++ 프로그래머스/2/42586. 기능개발/기능개발.js
@@ -12,7 +12,7 @@ const solution = (progresses, speeds) => {
     let count = 0;
     let cur = result[0];
     const ans = [];
-    
+
     for(let elem of result){
         if(elem <= cur){
             count++;
@@ -23,11 +23,5 @@ const solution = (progresses, speeds) => {
         }
     }
     ans.push(count);
-    
     return ans;
 }
-
-//1. progresses의 인자마다 (speeds의 인자 * a)를 했을 때 a를 몇으로 해야 100 이하가 되는지 구한다.
-//2. count값을 result 배열에 넣는다.
-//3. for문으로 return 값을 구한다.
-//*이중 for문 돌려도 괜찮다
