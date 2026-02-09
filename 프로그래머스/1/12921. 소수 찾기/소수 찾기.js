84e164b8ecd47a932ff315acaebb6a53ab898342
IlWooChoi <ilwoo.choi.00@gmail.com>
2026-02-09 11:08:22 +0900

    [level 1] Title: 소수 찾기, Time: 52.42 ms, Memory: 44.9 MB -BaekjoonHub

---
+++ 프로그래머스/1/12921. 소수 찾기/소수 찾기.js
@@ -0,0 +1,16 @@
+function solution(n) {
+    let sieve = new Array(n + 1).fill(true);
+    
+    sieve[0] = false;
+    sieve[1] = false;
+    
+    for (let i = 2; i <= Math.sqrt(n); i++) {
+        if (sieve[i]) {
+            for (let j = i * i; j <= n; j += i) {
+                sieve[j] = false;
+            }
+        }
+    }
+    
+    return sieve.filter(isPrime => isPrime).length;
+}
