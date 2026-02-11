7fbe6663693940eef0807c351658d39a29af19c4
IlWooChoi <ilwoo.choi.00@gmail.com>
2026-02-11 17:36:15 +0900

    [level 1] Title: 폰켓몬, Time: 2.11 ms, Memory: 34.2 MB -BaekjoonHub

--- 프로그래머스/1/1845. 폰켓몬/폰켓몬.js
+++ 프로그래머스/1/1845. 폰켓몬/폰켓몬.js
@@ -1,9 +1,18 @@
-const solution = (nums) => {
-    let lgth = nums.length;
-    let hi = new Set();
-    for(let i = 0; i < lgth; i++){
-        hi.add(nums[i]);
+function solution(nums) {
+    let pickLimit = nums.length / 2;
+    let hash = {};
+    let result = 0;
+
+    for (let i = 0; i < nums.length; i++) {
+        let currentType = nums[i];
+
+        if (hash[currentType] === undefined) {
+            if (result < pickLimit) {
+                result += 1;
+            }
+            hash[currentType] = true;
+        }
     }
     
-    return hi.size <= (lgth/2) ? hi.size : lgth/2;
+    return result;
 }
